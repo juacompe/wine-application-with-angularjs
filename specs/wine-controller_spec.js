9eb01b2b8be40841b6b08750e3916c798d2cdab3
juacompe <juacompe@gmail.com>
2013-10-09 13:10:15 +0700

    - add underscore - change spec so that there is only 1 describe (top level) - add deleteById and deleteButtonPressed - about to add bootbox lib

--- specs/wine-controller_spec.js
+++ specs/wine-controller_spec.js
@@ -10,35 +10,34 @@ describe('WineController', function() {
 
     it('should have 3 items after initialized', function() {
         expect($scope.wines.length).toEqual(3);
+    });
 
-        describe('first wine', function() {
-            it('should be LILLY BOOT, 2012', function() {
-                var firstWine = $scope.wines[0];
-                expect(firstWine.id).toEqual(1);
-                expect(firstWine.name).toEqual('LILLY BOOT');
-                expect(firstWine.year).toEqual(2012);
-            });
-        });
+    it('should have LILLY BOOT, 2012 as first wine', function() {
+        var firstWine = $scope.wines[0];
+        expect(firstWine.id).toEqual(1);
+        expect(firstWine.name).toEqual('LILLY BOOT');
+        expect(firstWine.year).toEqual(2012);
+    });
 
-        describe('second wine', function() {
-            it('should be LILLY BOOT, 2012', function() {
-                var secondWine = $scope.wines[1];
-                expect(secondWine.id).toEqual(2);
-                expect(secondWine.name).toEqual('ROOKS');
-                expect(secondWine.year).toEqual(2012);
-            });
-        });
+    it('should have ROOKS, 2012 as second wine', function() {
+        var secondWine = $scope.wines[1];
+        expect(secondWine.id).toEqual(2);
+        expect(secondWine.name).toEqual('ROOKS');
+        expect(secondWine.year).toEqual(2012);
+    });
 
-        describe('third wine', function() {
-            it('should be LILLY BOOT, 2012', function() {
-                var thirdWine = $scope.wines[2];
-                expect(thirdWine.id).toEqual(3);
-                expect(thirdWine.name).toEqual('HESKETH');
-                expect(thirdWine.year).toEqual(2009);
-            });
-        });
-    }); // initialize
+    it('should have HESKETH, 2009 as third wine', function() {
+        var thirdWine = $scope.wines[2];
+        expect(thirdWine.id).toEqual(3);
+        expect(thirdWine.name).toEqual('HESKETH');
+        expect(thirdWine.year).toEqual(2009);
+    });
 
+    it('should have only 2 wines left after delete first wine', function() {
+        var wineId = 1;
+        $scope.deleteById(wineId);
+        expect($scope.wines.length).toEqual(2);
+    });
 });
 
 
--- static/js/wine-controller.js
+++ static/js/wine-controller.js
@@ -4,6 +4,19 @@ function WineController($scope) {
         {id: 2, name: 'ROOKS', year: 2012},
         {id: 3, name: 'HESKETH', year: 2009},
     ]; 
-}
 
+    $scope.deleteById = function(wineId) {
+        $scope.wines = _.reject($scope.wines, function(wine){
+            return wine.id == wineId;
+        });
+    };
+
+    $scope.deleteButtonPressed = function(wineId) {
+        var confirm = $('#myModal').modal('show');
+        console.log(confirm);
+        if(confirm) {
+            $scope.deleteById(wineId);
+        }
+    };
+}
 
